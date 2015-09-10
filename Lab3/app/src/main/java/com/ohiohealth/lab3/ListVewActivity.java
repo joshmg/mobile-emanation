8f5f052b94198e7d6fe6a2aac0aa5d2d8291b4c1
gharraph <sherief@gharraph.com>
2015-09-10 13:58:21 -0400

    added listview to the second activity

--- Lab3/app/src/main/java/com/ohiohealth/lab3/ListVewActivity.java
+++ Lab3/app/src/main/java/com/ohiohealth/lab3/ListVewActivity.java
@@ -1,22 +1,46 @@
 package com.ohiohealth.lab3;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class ListVewActivity extends AppCompatActivity {
     private int numberOfCells;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_vew);
         numberOfCells = getIntent().getIntExtra(MainActivity.NUMBER_OF_CELLS, 0);
-        Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(numberOfCells), Toast.LENGTH_SHORT);
-        toast.show();
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[numberOfCells];
+        for (int i=0; i<numberOfCells; i++) {
+            values[i] = "cell number " + String.valueOf(i+1);
+        }
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.cell_row, R.id.cell_text, values);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+
+//        Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(numberOfCells), Toast.LENGTH_SHORT);
+//        toast.show();
     }
 
     @Override
