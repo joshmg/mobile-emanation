f3d8b3c4417ec61dc30da286a7741639f9de76a6
gharraph <sherief@gharraph.com>
2015-09-10 11:09:50 -0400

    added button, textview, edittext, and wired them up

--- Lab2/app/src/main/java/com/ohiohealth/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/ohiohealth/lab2/MainActivity.java
@@ -4,13 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button _button;
+    private TextView _staticText;
+    private EditText _editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _staticText = (TextView) findViewById(R.id.textView);
+        _editText = (EditText) findViewById(R.id.editText);
+        _button = (Button) findViewById(R.id.button);
+        _button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _staticText.setText(_editText.getText());
+            }
+        });
     }
 
     @Override
